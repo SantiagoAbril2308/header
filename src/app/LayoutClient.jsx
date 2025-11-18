a2e1e321538393818d885becf5a41435036debb5
brayanmac383-web <brayanmac383@gmail.com>
2025-11-18 16:07:38 -0500

    cambios

--- src/app/LayoutClient.jsx
+++ src/app/LayoutClient.jsx
@@ -1,39 +1,25 @@
-'use client'
+'use client';
 
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 import Header from "../components/ui/Header/Header"; 
 import Providers from '../components/Providers';
 import ToastNotification from '../components/Notification';
 
 const hiddenPaths = ['/loging', '/register'];
 
-
 export default function LayoutClient({ children }) {
+    const pathName = usePathname();
+    const hidden = hiddenPaths.includes(pathName);
 
-   
-    const pathName = usePathname();
-
-    const hidden = hiddenPaths.includes(pathName);
-
-    return (
-        // SOLUCIÓN: Providers envuelve toda la estructura para dar acceso al contexto.
-        <Providers> 
-            <div style={{
-                minHeight: "100vh",
-                display: "flex",
-                flexDirection: "column"
-            }}>
-
-                {/* Header (Ahora dentro de Providers y puede usar useCart) */}
-                {!hidden && <Header />}
-
-                <main style={{ flex: 1 }}>
-                    {children}
-                </main>
-                
-                {/* Toast (También dentro de Providers) */}
-                <ToastNotification /> 
-            </div>
+    return (
+        <Providers>
+            <div className="flex flex-col min-h-screen">
+                {!hidden && <Header />}
+                <main className="flex-1">
+                    {children}
+                </main>
+                <ToastNotification />
+            </div>
         </Providers>
-    )
+    );
 }
